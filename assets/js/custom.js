0bd1888bd7f38376c49b01d5b2fe146833b31d7e
Bo Lopker <lopkerk@gmail.com>
2021-02-17 15:44:18 -0800

    clean

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,44 +12,7 @@
 	  } else {
 	    $("header").removeClass("background-header");
 	  }
-	});
-	
-	$('.input-group.date').datepicker({format: "dd.mm.yyyy"});
-	
-
-	$('.filters ul li').click(function(){
-	  $('.filters ul li').removeClass('active');
-	  $(this).addClass('active');
-	  
-	  var data = $(this).attr('data-filter');
-	  $grid.isotope({
-	    filter: data
-	  })
-	});
-
-	var $grid = $(".grid").isotope({
-	  itemSelector: ".all",
-	  percentPosition: true,
-	  masonry: {
-	    columnWidth: ".all"
-	  }
-	})
-
-	$(".Modern-Slider").slick({
-	    autoplay:true,
-	    autoplaySpeed:10000,
-	    speed:600,
-	    slidesToShow:1,
-	    slidesToScroll:1,
-	    pauseOnHover:false,
-	    dots:true,
-	    pauseOnDotsHover:true,
-	    cssEase:'linear',
-	   // fade:true,
-	    draggable:false,
-	    prevArrow:'<button class="PrevArrow"></button>',
-	    nextArrow:'<button class="NextArrow"></button>', 
-	  });
+	});	
 
 	$('.search-icon a').on("click", function(event) {
 	    event.preventDefault();
@@ -71,32 +34,6 @@
 	    event.preventDefault();
 	    return false;
 	  });
-
-
-	$(function() {
-        $("#tabs").tabs();
-    });
-
-
-	$('.owl-menu-item').owlCarousel({
-		items:5,
-		loop:true,
-		dots: true,
-		nav: true,
-		autoplay: true,
-		margin:30,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:2
-			  },
-			  1000:{
-				  items:5
-			  }
-		  }
-	  })
 
 	// Window Resize Mobile Menu Fix
 	mobileNav();
